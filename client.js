500b60abe307510fb52923f9ff427eac907282ac
caozhihui <caozhihui@4399inc.com>
2018-08-27 11:24:29 +0800

    代码提交

---
+++ client.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+let net = require('net');
+let io = require('socket.io-client');
+let request = require('request');
+var prompt = require('prompt');
+const serverHost = "120.24.169.84";
+const serverPort = "3839";
+
+//
+// Start the prompt
+//
+prompt.start();
+
+//
+// Get two properties from the user: username and password
+//
+prompt.get([{
+    name: 'port',
+    message: '请输入您的端口号：如（8080）',
+    required: true
+}, {
+    name: 'key',
+    message: '请输入您的key ， 如果没有请打http://wwww.baidu.com 注册获取',
+    required: true,
+}], function (err, result) {
+    //
+    // Log the results.
+    //
+    new Client(result);
+    console.log('Command-line input received:');
+    console.log('  username: ' + result.username);
+    console.log('  password: ' + result.password);
+    return true;
+
+});
+
+
+class Client{
+    constructor({key,port}){
+        this.listenPort = port;
+        this.getHost(key);
+    }
+    getHost(key){
+        request.get(serverHost + ':4001/client/getHost?key='+key,(err,response,body)=>{
+            if(err){
+                console.log(err);
+                return false;
+            }
+            console.log(body);
+            let {code,result,message} = JSON.parse(body);
+            if(code === 100){
+                let {host} = result;
+                this.connect(host,key);
+            }
+            return console.log(message);
+        })
+    }
+    connect(host,key){
+        //var socket = io('http://120.24.169.84:3838');
+        let socket = io(`${serverHost}:${serverPort}?host=${host}&key=${key}`);
+        let client = {};
+
+        socket.on('connect', () => {
+            console.log('socket.io server connected');
+
+            socket.on('message', data => {
+                let {name,buffer} = data;
+                let clientFree = client[name];
+                if (!name) {
+                    return;
+                }
+                if (clientFree) {
+                    clientFree.write(buffer);
+                } else {
+                    clientFree = new net.Socket();
+                    clientFree.connect(this.listenPort, '127.0.0.1', function () {
+                        //写到浏览器
+                        clientFree.write(buffer);
+                    });
+                    clientFree.on('data', function (buf) {
+                        socket.emit('message', {
+                            name: name,
+                            buffer: buf
+                        })
+                    });
+                    clientFree.on('end', () => {
+                        socket.emit('message/end', {
+                            name: name,
+                            buffer: null
+                        })
+                    });
+                    clientFree.on('error', err => {
+                        console.log(err);
+                        socket.emit('message/end', {
+                            name: name,
+                            buffer: null
+                        })
+                    });
+                }
+            });
+        });
+    }
+}
+
+
+
